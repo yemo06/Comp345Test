ea461c0a1c0a58bf53df4b6afc825c6d381d6651
yemo06 <yemoafo@github.com>
2018-01-29 12:17:37 -0500

    First Commit

---
+++ src/edu/ithaca/afolabi/Main.java
@@ -0,0 +1,8 @@
+package edu.ithaca.afolabi;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/edu/ithaca/afolabi/StringCalculator.java
@@ -0,0 +1,8 @@
+package edu.ithaca.afolabi;
+
+public class StringCalculator {
+    String Add(String test){
+        return "";
+    }
+    
+}
